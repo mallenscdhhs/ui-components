18045a187a1cb2075fffd1fee164b13354588cb9
Michael Allen <michael@reverseloop.com>
2014-11-11 16:20:25 -0500

    RMMIS-1931: Grid now allows for nesting components as well as listing them in the config

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -22,10 +22,11 @@ var getColumnClassNames = function(col){
 var Grid = React.createClass({
 	propTypes: {
 		rows: React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.object)).isRequired,
-		components: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+		components: React.PropTypes.arrayOf(React.PropTypes.object)
 	},
 	render: function(){
 		var componentIndex = -1;
+		var components = this.props.components || this.props.children;
 		return (
 			<div className="grid-layout">
 			{this.props.rows.map(function(row, i){				
@@ -35,7 +36,7 @@ var Grid = React.createClass({
 							componentIndex+=1;
 							return(
 								<div className={getColumnClassNames(col)} key={"col-"+i+"-"+n}>
-									{this.props.components[componentIndex]}
+									{components[componentIndex]}
 								</div>
 							);
 						}, this)}
