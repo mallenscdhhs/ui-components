5017b82d647c6deaa812f69f932ec8576ee45de4
Michael Allen <michael@reverseloop.com>
2014-11-10 20:41:08 -0500

    RMMIS-1931: fix issue with Page and Layout components not loading in AMD build

---
+++ src/Form.jsx
@@ -0,0 +1,11 @@
+var React = require('react/addons');
+var _ = require('underscore');
+var Layout = require('./Layout');
+
+var Form = React.createClass({
+	render: function(){
+		return <form></form>;
+	}
+});
+
+module.exports = Form;
