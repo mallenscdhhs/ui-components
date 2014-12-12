fbd43b3fadfb60349b3a6d1b3586cabe7151e7f7
Michael Allen <michael@reverseloop.com>
2014-12-12 17:09:27 -0500

    RMMIS-2195: fix issue with build script trying to still export underscore instead of lodash

--- GulpFile.js
+++ GulpFile.js
@@ -38,13 +38,13 @@ gulp.task('transpile', ['clean'], function(){
 
 
 gulp.task('build:Components', ['transpile'], function(){
-  var underscorePath = require.resolve('underscore');
+  var underscorePath = require.resolve('lodash');
   var reactPath = require.resolve('react/addons');
   var markedPath = require.resolve('marked');
   return browserify(['./dist/build/main.js'])    
     .require([
       { file: './dist/build/main.js', expose: 'Components'},
-      { file: underscorePath, expose: 'underscore'},
+      { file: underscorePath, expose: 'lodash'},
       { file: reactPath, expose: 'react/addons'},
       { file: markedPath, expose: 'marked'}
     ])
