849106bea5306dc6d62e9049b9cb2f2bf289e57a
Michael Allen <michael@reverseloop.com>
2014-12-17 19:58:18 -0500

    RMMIS-2193: EditorMixin#handleConfigEdit and #handleConfigAdd both push the id property as their message data, add component type as a parameter to EditorMixin#getAddButton and move code that converted type to lowerCase to one place inside of #getEditController, fix whitespace issues

--- src/EditorMixin.js
+++ src/EditorMixin.js
@@ -2,46 +2,56 @@ var Queue = require('./EventQueue');
 
 module.exports = {
 
-    /**
-     * Stop even prop and push event to Queue, enabling global app to open the config editor window.
-     * @param e Event
-     */
-    handleConfigEdit: function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        Queue.push({'entityEvent':'component:edit:'+this.props.id,'data':{'id': this.props.id,'name':this.props.name,'props':this.props,'type':this.props.ComponentType}});
-    },
+  /**
+   * Stop event prop and push event to Queue, enabling global app to open the config editor window.
+   * We only need to send the component's "id" property to the config tool since it will already
+   * have the component config data.
+   * @param {object} e - Event object
+   */
+  handleConfigEdit: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    Queue.push({entityEvent: 'component:edit', data: this.props.id});
+  },
 
-    /**
-     * Stop even prop and push event to Queue, enabling global app to open the config editor window.
-     * @param e Event
-     */
-    handleConfigAdd: function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        Queue.push({'entityEvent':'component:add:new:'+this.props.id,'data':{'id': this.props.id,'name':this.props.name,'props':this.props,'type':this.props.ComponentType}});
-    },
+  /**
+   * Stop even prop and push event to Queue, enabling global app to open the config editor window.
+   * @param e Event
+   */
+  handleConfigAdd: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    Queue.push({entityEvent:'component:add:new', data: this.props.id});
+  },
 
-    getAddButton: function(){
-      var addButton;
-      if(this.props.ComponentType.toLowerCase() !== 'field' &&
-          this.props.ComponentType.toLowerCase() !== 'action'){
-          addButton = <span onClick={this.handleConfigAdd} className="add-component"><span className="glyphicon glyphicon glyphicon-plus"></span></span>;
-      }
-      return addButton;
-    },
+  /**
+   * Only show an add button if the component can have children(which 'field' and 'action' cannot). 
+   * @param {string} type - the type of component
+   * @returns {JSX}
+   */
+  getAddButton: function(type){    
+    if(type !== 'field' && type !== 'action'){
+      return (
+        <span onClick={this.handleConfigAdd} className="add-component">
+          <span className="glyphicon glyphicon glyphicon-plus"></span>
+        </span>
+      );
+    }    
+  },
 
-    /**
-     * Create edit component html handler.
-     * @param componentName
-     * @returns {JSX Template}
-     */
-    getEditController: function (componentType) {
-        this.props.ComponentType = componentType.toLowerCase();
-        return  <div className="config-editor">
-                    {this.getAddButton()}
-                    <span onClick={this.handleConfigEdit}  className="edit-component"><span className="glyphicon glyphicon-cog"></span></span>
-                </div>;
-    }
-
-}
+  /**
+   * Create edit component html handler.
+   * @param {string} 
+   * @returns {JSX Template}
+   */
+  getEditController: function() {    
+    return (
+      <div className="config-editor">
+        {this.getAddButton(this.props.type.toLowerCase())}
+        <span onClick={this.handleConfigEdit} className="edit-component">
+          <span className="glyphicon glyphicon-cog"></span>
+        </span>
+      </div>
+    );
+  }
+};
