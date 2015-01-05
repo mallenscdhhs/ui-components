9738500598a9b34bb7cbe3a17dcdb90d9754dd5c
Michael Allen <michael@reverseloop.com>
2015-01-05 16:03:48 -0500

    RMMIS-2368: update comments in AutoComplete, remove old test files, add src/styles for less compilation, update build tasks to include compiling less & copying bootstrap fonts, add bootstrap as a dependency, update release script

--- src/AutoComplete.jsx
+++ src/AutoComplete.jsx
@@ -4,12 +4,24 @@ var OptionsMixin = require('./OptionsMixin');
 var Queue = require('./EventQueue');
 var _ = require('lodash');
 
+/**
+ * Options are configured as an array of objects, but the Typeahead
+ * component expects a list of strings. This function converts an
+ * array of Option configs to an array of the Option.label values.
+ * @param {array} options - an array of option config objects
+ * @returns {array} an array of strings
+ */
 var getOptionLabels = function(options){
   return _.map(options, function(option){
     return option.label;
   });
 };
 
+/**
+ * Autocomplete field. Will filter a list of options based on user input.
+ * This wraps the react-typeahead#Typeahead component from https://github.com/fmoo/react-typeahead.
+ * @module AutoComplete
+ */
 module.exports = React.createClass({
 
   displayName: 'AutoComplete',
@@ -27,8 +39,15 @@ module.exports = React.createClass({
 
   mixins: [OptionsMixin],
 
+  statics: {
+    getOptionLabels: getOptionLabels
+  },
+
   /**
-   *
+   * When the user chooses an option from the list, this method is called
+   * with the selection as param.
+   * @param {string} label - the user's selection
+   * @fires field:value:change
    */
   onOptionSelected: function(label){
     var opt = _.find(this.state.options, {label: label});
@@ -43,12 +62,18 @@ module.exports = React.createClass({
     });
   },
 
+  /**
+   * Renders the auto complete markup. If we have not loaded the options
+   * yet then we render a placeholder. When the options are finished loading
+   * we will render the actual Typeahead component.
+   * @returns {JSX}
+   */
   render: function(){
     if ( this.state.options.length ){
       return (
         <Typeahead
           ref="typeahead"
-          options={getOptionLabels(this.state.options)} 
+          options={getOptionLabels(this.state.options)}
           className="field-autocomplete"
           onOptionSelected={this.onOptionSelected}
           customClasses={{
