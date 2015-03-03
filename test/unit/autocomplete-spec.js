a8f288911faf3c468b217494b23073812f167d0e
John Schulz <john@Johns-MacBook-Pro-13.local>
2015-03-03 14:46:18 -0500

    RMMIS-2871: Updated the test for the Autocomplete component

--- test/unit/autocomplete-spec.js
+++ test/unit/autocomplete-spec.js
@@ -9,24 +9,34 @@ describe('AutoComplete', function(){
   var fixture = require('../fixtures/autocomplete.json');
   var component = TestUtils.renderIntoDocument(<AutoComplete {...fixture.config}/>);
   var dom = component.getDOMNode();
-  var inpt = TestUtils.findRenderedDOMComponentWithClass(component, 'form-control');
+  var inpt = TestUtils.findRenderedDOMComponentWithClass(component, 'rw-input');
   var inptDom = inpt.getDOMNode();
 
   it('can render', function(done){
-    expect(dom.className).toEqual('typeahead field-autocomplete');
-    expect(inptDom.className).toEqual('form-control');
+    expect(dom.className).toEqual('field-autocomplete rw-combobox rw-widget');
+    expect(inptDom.className).toEqual('rw-input rw-input');
     setTimeout(function(){
-      expect(component.refs.typeahead.state.options.length).toEqual(5);
+      expect(component.state.options.length).toEqual(5);
       done();
     }, 300);
   });
 
+  it('will update options based on input value', function(){
+    component = TestUtils.renderIntoDocument(<AutoComplete {...fixture.config} value='f'/>);
+    var compBtn = TestUtils.findRenderedDOMComponentWithClass(component, 'rw-select');
+    TestUtils.Simulate.click(compBtn);
+    var listItems = TestUtils.scryRenderedDOMComponentsWithClass(component, 'rw-list-option');
+
+    expect(listItems.length).toEqual(2);
+  });
+
   it('will call FIELD_VALUE_CHANGE action after user selection', function(done){
-    var node = component.refs.typeahead.refs.entry.getDOMNode();
+    var node = inptDom;
     node.value = 'f';
     TestUtils.Simulate.change(node);
-    var listItems = TestUtils.scryRenderedDOMComponentsWithClass(component, 'list-group-item');
-    expect(listItems.length).toEqual(2);
+
+    var listItems = TestUtils.scryRenderedDOMComponentsWithClass(component, 'rw-list-option');
+    console.log('listItems', listItems[0].getDOMNode());
 
     Dispatcher.register('ac-test', function(action,data){
       if ( action === constants.actions.FIELD_VALUE_CHANGE ) {
