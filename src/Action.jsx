f22c864ce74dae4cc22903dea24f3e01576f12f9
John Schulz <john@ampleinnovation.com>
2015-04-14 16:32:48 -0400

    RMMIS-3434: For discussion about React .13 WARNs based on our action component

--- src/Action.jsx
+++ src/Action.jsx
@@ -23,40 +23,6 @@ module.exports = React.createClass({
   },
 
   /**
-  * Returns an object for action configuration
-  * @return {Object}
-  */
-  getConfig: function() {
-    var base = {
-      key: this.props.id+'-action',
-      className: this.getClasses(),
-      href: this.props.url
-    };
-
-    return (this.props.type === 'link') ? _.merge(this.props, base) :  _.merge(this.props, _.omit(base, 'href'));
-  },
-
-  /**
-  * Returns a string of classes
-  * @return {String}
-  */
-  getClasses: function(){
-    var classes = ['btn'];
-    // Add default link-type for action links
-    if(this.props.type==='link'){
-      classes.push('btn-link');
-    }
-    // Add all passed in classes
-    if(this.props.classNames){
-      classes = classes.concat(this.props.classNames);
-    }
-    if(this.props.disabled === true){
-      classes.push('disabled');
-    }
-    return classes.join(' ');
-  },
-
-  /**
   * Returns a span element with icon classes
   * @return {Object}
   */
@@ -83,10 +49,8 @@ module.exports = React.createClass({
   * @return {JSX Template}
   */
   getLink: function(){
-    var props = this.getConfig();
-
     return (
-      <a {...props} onClick={this.handleClick}>
+      <a {...this.props} onClick={this.handleClick} href={this.props.url}>
         {this.getIcon()}
         {this.props.name}
       </a>
@@ -98,10 +62,8 @@ module.exports = React.createClass({
   * @return {JSX Template}
   */
   getButton: function(){
-    var props = this.getConfig();
-
     return (
-      <button {...props} onClick={this.handleClick}>
+      <button {...this.props} onClick={this.handleClick}>
         {this.getIcon()}
         {this.props.name}
       </button>
--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,26 @@ exports.containsOneOf = function(list, values){
   if ( !values.length ) return false;
   return _.contains(list, _.first(values)) || exports.containsOneOf(list, _.rest(values));
 };
+
+/**
+* Returns a string of classes
+* @param {object} props - Abstract the classNames array, type string,
+* and disabled boolean from the parent component to determine which
+* classes should be passed to the owner component
+* @return {String}
+*/
+exports.getClasses = function(props){
+  var classes = ['btn'];
+  // Add default link-type for action links
+  if(props.type === 'link'){
+    classes.push('btn-link');
+  }
+  // Add all passed in classes
+  if(props.classNames){
+    classes = classes.concat(props.classNames);
+  }
+  if(props.disabled === true){
+    classes.push('disabled');
+  }
+  return classes.join(' ');
+};
