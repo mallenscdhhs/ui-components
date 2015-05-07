c14ba7734f7c6de18449d1a94d40b90688aa6f53
Michael Allen <michael.allen@scdhhs.gov>
2015-05-07 13:53:43 -0400

    RMMIS-5102: fix main spec

--- test/unit/main-spec.js
+++ test/unit/main-spec.js
@@ -1,45 +1,26 @@
 var React = require('react');
 var Components = require('../../src/main');
 var TestUtils = require('react/lib/ReactTestUtils');
+var config = require('../../src/configuration');
 
 describe('main component', function(){
+  it('exposes the list of elements', function(){
+    expect(Components.elements).toBeDefined();
+  });
+  it('exposes the constants', function(){
+    expect(Components.constants).toBeDefined();
+  });
+  it('allows for configuration', function(){
+    expect(Components.configure).toBeDefined();
+    Components.configure({foo: 'bar'});
+    expect(config.foo).toEqual('bar');
+  });
   describe('#factory', function(){
     it('can build the ReactElement DOM from schema', function(){
       var fixture = require('../fixtures/main.json');
       var Comp = Components.factory(fixture);
       var page = TestUtils.renderIntoDocument(Comp);
       expect(page.getDOMNode()).toBeDefined();
-    });
-    it('will create a copy of the passed-in data', function(){
-      var fixture = require('../fixtures/main.json');
-      var Comp = Components.factory(fixture);
-      expect(fixture.components.test_field_1.config.component_id).not.toBeDefined();
-    });
-    it('will throw an error if no componentHead is supplied with a components list', function(){
-      var fixture = require('../fixtures/main-no-componentHead.json');
-      expect(Components.factory.bind(null, fixture)).toThrow();
-    });
-  });
-  describe('#buildComponentTree', function(){
-    it('can build a nested data structure for rendering Component tree', function(){
-      var fixture = require('../fixtures/main.json');
-      var page = Components.buildComponentTree(fixture, fixture)[0]._store.props;
-      expect(page.children.length).toEqual(2);
-      var form = page.children[0]._store.props;
-      expect(form.children.length).toEqual(1);
-      var fieldset = form.children[0]._store.props;
-      expect(fieldset.children.length).toEqual(1);
-      var fieldsetLayout = fieldset.children[0]._store.props;
-      expect(fieldsetLayout.children.length).toEqual(2);
-      var field1 = fieldsetLayout.children[0]._store.props;
-      var field2 = fieldsetLayout.children[1]._store.props;
-      var action = page.children[1]._store.props;
-
-      expect(form.id).toEqual('test_form');
-      expect(fieldset.id).toEqual('test_fieldset');
-      expect(field1.id).toEqual('test_field_1');
-      expect(field2.id).toEqual('test_field_2');
-      expect(action.id).toEqual('action_1');
     });
   });
 });
