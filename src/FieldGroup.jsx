d973b4cbfc8e5c61c05c152553322fda304a32ae
John Schulz <john@ampleinnovation.com>
2015-06-16 14:46:59 -0400

    RMMIS-5503: Removed if statement again, changed value to be value, removed fieldGroupValue, and added individualValue

--- src/FieldGroup.jsx
+++ src/FieldGroup.jsx
@@ -42,7 +42,11 @@ module.exports = React.createClass({
     persistInSession: React.PropTypes.bool
   },
 
-
+  getDefaultProps: function() {
+    return {
+      fieldValueChangeAction: constants.actions.FIELD_VALUE_CHANGE
+    };
+  },
 
   getInitialState: function(){
     return {
@@ -64,17 +68,8 @@ module.exports = React.createClass({
           }
         }
         this.setState({'value': value});
-        if(this.props.fieldValueChangeAction) {
-          Flux.doAction( this.props.fieldValueChangeAction, _.merge(this.props, {fieldGroupValue: value, value: data.value}) );
-        } else {
-          Flux.doAction(constants.actions.FIELD_VALUE_CHANGE, {
-            id: this.props.id,
-            name: this.props.name,
-            value: value,
-            rules : this.props.rules,
-            persistInSession: this.props.persistInSession
-          });
-        }
+        // added individualValue to represent the latest individual value changed in a field-group
+        Flux.doAction( this.props.fieldValueChangeAction, _.merge(this.props, {individualValue: data.value, value: value}) );
       }
     }.bind(this));
   },
