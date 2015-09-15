19ca54dc9c56327634df7c27b20464bc23400269
John Schulz <john@ampleinnovation.com>
2015-09-15 16:05:26 -0400

    RMMIS-6290: Fixed typo

--- test/unit/content-spec.js
+++ test/unit/content-spec.js
@@ -10,7 +10,7 @@ describe('Content component', () => {
   let content = TestUtils.renderIntoDocument(<Content {...config}/>);
   let dom = React.findDOMNode(content);
 
-  it('renders a bootstrap accordion', () => {
+  it('renders content', () => {
     expect(dom.childNodes[0].textContent).toEqual('I am some ');
     expect(dom.childNodes[1].textContent).toEqual('content');
     expect(dom.childNodes[1].tagName).toEqual('B');
