702dc99c34031308059b2dbe8e0b66c9e55caf07
John Schulz <john@ampleinnovation.com>
2015-09-23 23:58:49 -0400

    RMMIS-6458: Update File component

--- test/unit/file-spec.js
+++ test/unit/file-spec.js
@@ -1,124 +1,71 @@
 import React from 'react';
-import Flux, { dispatcher as Dispatcher } from 'fluxify';
-import constants from '../../src/constants';
 import TestUtils from 'react/lib/ReactTestUtils';
 import File from '../../src/File';
+import _ from 'lodash';
 
 let fixture = require('../fixtures/file.json');
-let {
-  FIELD_VALUE_CHANGE,
-  FILE_PREVIEW_LIST_GET,
-  FILE_PREVIEW_LIST_LOAD,
-  FILE_PREVIEW_LIST_SEND,
-  FILE_PREVIEW_LIST_REMOVE
-} = constants.actions;
 
 describe('File', () => {
-  let config = {id: 'file-test', name: 'fileTest', value: fixture};
-  let configTwo = {id: 'file-test2', name: 'fileTest2', value: ''};
+  let config = _.omit(fixture, 'file');
   let comp = TestUtils.renderIntoDocument(<File {...config}/>);
-  let wrapper = React.findDOMNode(comp);
-  let ul = wrapper.childNodes[0];
-  let uploadField = wrapper.childNodes[1];
-  let firstLi = ul.childNodes[0];
-  let fileName = firstLi.childNodes[0].childNodes[0];
-  let removeLink = firstLi.childNodes[0].childNodes[1];
-  let previewImg = TestUtils.scryRenderedDOMComponentsWithTag(firstLi, 'img');
 
-  it('renders a file preview', () => {
-      expect(firstLi.className).toEqual('file-preview-list-item row mblg');
-      expect(fileName.textContent).toEqual('tiny.gif');
-
-      expect(previewImg.src).toEqual(fixture.binary);
+  it('renders an input with a type of file', () => {
+    let input = TestUtils.findRenderedDOMComponentWithTag(comp, 'input');
+    let dom = React.findDOMNode(comp);
+    expect(input).toBeDefined();
+    expect(dom.type).toEqual('file');
   });
 
-  xit('can configure maximum number of files to be uploaded', () => {
-      expect(wrapper.childNodes[1].id).toEqual('file-test');
-      // set this.props.limit to 1 (file uploaded earlier will bring us up to max)
-      comp.props.limit = 1;
-      // re-render with the new limit applied
-      comp.setState();
-      expect(wrapper.childNodes[1].id).toEqual('limit-message');
-  });
-
-  it('fires FIELD_VALUE_CHANGE with an array of uploaded files and attempts to persist via API', (done) => {
-    let fvcEvent = [
-      {
-        "name": "tiny2.png",
-        "size": 101,
-        "lastModified": 1431441313001,
-        "lastModifiedDate": "Date 2015-05-12T14:35:13.000Z",
-        "type": "image/png",
-        "binary": ""
-      },
-      {
-        "name": "tiny3.jpg",
-        "size": 1101,
-        "lastModified": 1431441313002,
-        "lastModifiedDate": "Date 2015-05-12T14:35:13.000Z",
-        "type": "image/jpg",
-        "binary": ""
-      }
-    ];
-    Dispatcher.register('file-upload-change' , (action, data) => {
-      if ( action === FIELD_VALUE_CHANGE && data.id === 'file-test') {
-        Dispatcher.unregister('file-upload-change');
-        expect(data.value).toEqual(fvcEvent);
-      }
+  it('can pass an uploaded file to the application to be persisted', () => {
+    let uploadedFile = _.omit(fixture.file, 'binary');
+    let e = {target: {files: [uploadedFile]}};
+    comp.handleInputChange(e);
+    expect(e.component.file).toEqual({
+      id: comp.props.id,
+      name: comp.props.name,
+      properties: uploadedFile
     });
-    Dispatcher.register('send-file-preview-list', (action, data) => {
-      if (action === FILE_PREVIEW_LIST_SEND) {
-        Dispatcher.unregister('send-file-preview-list');
-        expect(action).toEqual('sendFilePreviewList');
-        done();
-      }
-    });
-    comp.buildChangeEvent(fvcEvent);
   });
 
-  it('will attempt to fetch files via an API if none are supplied in props', (done) => {
-    Dispatcher.register('get-file-preview-list', (action, data) => {
-      if (action === FILE_PREVIEW_LIST_GET && data.fieldId === configTwo.id) {
-        Dispatcher.unregister('get-file-preview-list');
-        expect(action).toEqual('getFilePreviewList');
-        expect(data.fieldId).toEqual('file-test2');
-        done();
-      }
-    });
-    let compTwo = TestUtils.renderIntoDocument(<File {...configTwo}/>);
-  });
-
-  it('can load files for preview fetched from an API', (done) => {
-    let compTwo = TestUtils.renderIntoDocument(<File {...configTwo}/>);
-    expect(compTwo.value).not.toBeDefined();
-
-    Dispatcher.register('load-file-preview-list', (action, data) => {
-      if (action === FILE_PREVIEW_LIST_LOAD && data.fieldId === configTwo.id) {
-        Dispatcher.unregister('load-file-preview-list');
-        expect(compTwo.value).toEqual(fixture.value);
-        done();
-      }
-    });
-    // Action called in PE on successful response from FILE_PREVIEW_LIST_GET
-    Flux.doAction(FILE_PREVIEW_LIST_LOAD, {fieldId: 'file-test2', value: fixture});
-  });
-
-  it('will not allow field value changes or API operations to occur if readOnly is true', () => {
-    let removalFVCEvent = {id: 'remove-link', type: 'link', name: 'remove', className: 'text-left', fileId: undefined, removalId: 0, dataParent: 'file-test', event: 'removeFilePreviewList', componentType: 'action'};
-
-    spyOn(Flux, 'doAction');
-    comp.props.readOnly = true;
-    // re-render with readOnly applied
+  it('can render a link to preview the file in a new tab when provided in the schema', () => {
+    // simulate re-render from schemaUpdate
+    comp.props.file = fixture.file;
     comp.setState();
-    // click the Remove link
-    TestUtils.Simulate.click(removeLink);
-    expect(ul.class).not.toBeDefined();
-    expect(Flux.doAction).not.toHaveBeenCalledWith('removeFilePreviewList', removalFVCEvent);
-    //reset readOnly to default for other tests
-    comp.props.readOnly = false;
-    // re-render with readOnly applied
-    comp.setState();
+    let input = TestUtils.scryRenderedDOMComponentsWithTag(comp, 'input');
+    let previewLink = TestUtils.findRenderedDOMComponentWithTag(comp, 'a');
+    let dom = React.findDOMNode(previewLink);
+    expect(_.isEmpty(input)).toBe(true);
+    expect(previewLink).toBeDefined();
+    expect(dom.id).toEqual(`preview-link-${fixture.id}`);
+    expect(dom.href).toEqual(comp.props.file.binary);
   });
 
+  it('can render an edit link to handle changes to previously uploaded files', () => {
+    // simulate re-render from schemaUpdate
+    comp.props.file = fixture.file;
+    comp.setState();
+    let dom = React.findDOMNode(comp);
+    let editLink = TestUtils.findRenderedDOMComponentWithTag(comp, 'button');
+    dom = React.findDOMNode(editLink);
+    expect(editLink).toBeDefined();
+    expect(dom.id).toEqual(`edit-link-${fixture.id}`);
+    expect(comp.props.readOnly).toBe(false);
+    let e = {nativeEvent: {type: 'click'}};
+    comp.handleClick(e);
+    expect(e.component).toEqual({
+      id: comp.props.id,
+      schemaUpdates: {
+        file: {},
+      }
+    });
+
+    // simulate re-render from schemaUpdate
+    comp.props.file = e.component.schemaUpdates.file;
+    comp.setState();
+    let input = TestUtils.findRenderedDOMComponentWithTag(comp, 'input');
+    let inputInDOM = React.findDOMNode(input);
+    expect(inputInDOM).toBeDefined();
+    expect(inputInDOM.type).toEqual('file');
+  });
 
 });
