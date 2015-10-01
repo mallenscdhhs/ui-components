6645bfdec53099c7d625f8f002cbaee2d27d390e
Michael Allen <michael.allen@scdhhs.gov>
2015-10-01 09:10:15 -0400

    RMMIS-6532: add validation error styles to FieldGroup, add help-block text to FieldGroup

--- src/FieldGroup.jsx
+++ src/FieldGroup.jsx
@@ -4,6 +4,7 @@ import Immutable from 'immutable';
 import {Input} from 'react-bootstrap';
 import FieldLabel from './FieldLabel';
 import _ from 'lodash';
+import classNames from 'classnames';
 
 /**
  * Represents a group of Checkable instances. Will manage value changes and blurs.
@@ -56,11 +57,23 @@ class FieldGroup extends React.Component {
     };
   }
 
+  renderHelpBlock(message) {
+    if (message) {
+      return <p className="help-block">{message}</p>;
+    }
+  }
+
   render() {
+    let styles = classNames({
+      'has-error': this.props.hasError,
+      'form-group': true
+    });
+    
+    let message = this.props.message || this.props.helpText;
     return (
-      <fieldset onChange={this.handleChange} onBlur={this.props.onBlur}>
+      <fieldset className={styles} onChange={this.handleChange} onBlur={this.props.onBlur}>
         <legend>
-          <FieldLabel {...this.props}/>
+          {this.props.label}
         </legend>
         <div className="field-group">
           {_.map(this.props.options, (option) => {
@@ -78,6 +91,7 @@ class FieldGroup extends React.Component {
 
             return <Input {...config}/>;
           })}
+          {this.renderHelpBlock(message)}
         </div>
       </fieldset>
     );
@@ -86,11 +100,20 @@ class FieldGroup extends React.Component {
 
 FieldGroup.propTypes = {
   id: React.PropTypes.string.isRequired,
-  name: React.PropTypes.string.isRequired,
   type: React.PropTypes.string.isRequired,
-  value: React.PropTypes.any,
+  name: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
   required: React.PropTypes.bool,
-  persistInSession: React.PropTypes.bool
+  helpText: React.PropTypes.string,
+  visible: React.PropTypes.bool,
+  disabled: React.PropTypes.bool
+};
+
+FieldGroup.defaultProps = {
+  initialState: 'visible',
+  disabled: false,
+  visible: true,
+  hasError: false
 };
 
 export default FieldGroup;
